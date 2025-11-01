8ada74bbc093d11b04ecc9180fd52bdb76a773bc
ikmalfalahi <ikmalfalahi99@gmail.com>
2025-11-01 11:29:37 +0700

    Update profil.js

--- assets/js/profil.js
+++ assets/js/profil.js
@@ -1,6 +1,6 @@
 // === Konfigurasi Supabase ===
-const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://sosjorfcrsktcitaawyi.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvc2pvcmZjcnNrdGNpdGFhd3lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5NjcyMjcsImV4cCI6MjA3NzU0MzIyN30.4u1fZs46awWRQve_lfQGHE0bxP4Kqbv8qwhDtBogBUQ";
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // === Ambil Elemen HTML ===
@@ -132,3 +132,4 @@ logoutBtn.addEventListener("click", async () => {
 
 // === Jalankan ===
 checkSession();
+
