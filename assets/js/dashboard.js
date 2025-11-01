78b3d6204792306c588e22a522283707be8d5d5c
ikmalfalahi <ikmalfalahi99@gmail.com>
2025-11-01 11:38:33 +0700

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function bindLogout() {
     logoutBtns.forEach(b => b && b.addEventListener("click", async () => {
       await supabase.auth.signOut();
-      window.location.href = "ikmalfalahi.git.io/putra-delima/login.html";
+      window.location.href = "https://ikmalfalahi.github.io/putra-delima/index.html";
     }));
   }
 
@@ -617,4 +617,5 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
 
