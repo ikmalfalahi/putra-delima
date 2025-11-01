f5e1fc581542c10cf2179a46fd8ecdbfcff68ca1
ikmalfalahi <ikmalfalahi99@gmail.com>
2025-11-01 10:59:54 +0700

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,5 +1,5 @@
 // === Supabase Config ===
-const SUPABASE_URL = "https://fubxhouemqkixovtqcxp.supabase.co"; // ganti dengan URL Supabase kamu
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1Ynhob3VlbXFraXhvdnRxY3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5Mjk3OTYsImV4cCI6MjA3NzUwNTc5Nn0.1DpX6SroRAZtU92R6-BsguC700gNUpUrjBeI7OR4uqY"; // ganti dengan anon key kamu
+const SUPABASE_URL = "https://nnohtnywmhuzueamsats.supabase.co"; // ganti dengan URL Supabase kamu
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ub2h0bnl3bWh1enVlYW1zYXRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwNjM4NDksImV4cCI6MjA3NDYzOTg0OX0.S8FeDIdXQ32WH9QPVlSsYGRjxYbLMg6HXQicZ35A1pg"; // ganti dengan anon key kamu
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
